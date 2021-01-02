e553b46d76ed0a19e8a1d9540cf570f8cfb0a1fd
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-01-02 11:09:11 -0600

    Initial components and setup

--- MetasVentasApp/src/app/app.module.ts
+++ MetasVentasApp/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
