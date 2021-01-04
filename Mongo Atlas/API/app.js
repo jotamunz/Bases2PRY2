6f4e8c78c464fda186cb7b646b74576646bae72c
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-01-04 16:41:49 -0600

    Basic DB connection

---
+++ Mongo Atlas/API/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+
+app.use(
+	express.urlencoded({
+		extended: true
+	})
+);
+app.use(express.json());
+
+mongoose.connect(
+	process.env.DB_CONNECTION,
+	{ useUnifiedTopology: true, useNewUrlParser: true },
+	() => {
+		console.log('connected to DB');
+	}
+);
+
+// Server start
+app.listen(3000, () => console.log('Server started on port 3000'));
