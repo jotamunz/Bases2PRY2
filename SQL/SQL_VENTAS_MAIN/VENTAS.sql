a9b7e7bb160df65415f99f0c9bb96a06e18eb4d7
Joseph <jostenoriop@gmail.com>
2021-01-06 17:40:08 -0600

    Created sales tables and Integration Services project

---
+++ SQL/SQL_VENTAS_MAIN/VENTAS.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[VENTAS]
+(
+	[Id] INT NOT NULL, 
+    [Factura] INT NOT NULL, 
+    [Fecha] DATE NOT NULL, 
+    [FechaVencimiento] DATE NOT NULL, 
+    [Cliente] NCHAR(5) NOT NULL, 
+    [Producto ] NCHAR(6) NOT NULL, 
+    [Cantidad] INT NOT NULL, 
+    [Moneda] NCHAR(3) NOT NULL, 
+    [Precio] DECIMAL(20, 4) NOT NULL, 
+    [Total] DECIMAL(20, 4) NOT NULL, 
+    [Almacen] INT NOT NULL, 
+    [Vendedor] DECIMAL(5) NOT NULL, 
+    [TotalUSD] DECIMAL(20, 4) NOT NULL, 
+    [Impuesto] DECIMAL(20, 4) NOT NULL, 
+    [ImpuestoUSD] DECIMAL(20, 4) NOT NULL, 
+    [TipoDeCambio] DECIMAL(20, 4) NOT NULL, 
+    [Ganancia] DECIMAL(20, 4) NOT NULL, 
+    [GananciaUSD] DECIMAL(20, 4) NOT NULL,
+    CONSTRAINT PK_VENTAS PRIMARY KEY (Id)
+)
