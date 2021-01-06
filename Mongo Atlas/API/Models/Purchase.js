239f2e416a31abd3c4f2e16e782a277429062b74
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-01-05 19:24:14 -0600

    Validated model

--- Mongo Atlas/API/Models/Purchase.js
+++ Mongo Atlas/API/Models/Purchase.js
@@ -4,15 +4,21 @@ const ArticleSchema = mongoose.Schema(
 	{
 		articleCode: {
 			type: String,
-			required: true
+			required: true,
+			match: /A[0-9]{6}/
 		},
 		quantity: {
 			type: Number,
-			required: true
+			required: true,
+			min: 0,
+			validate: {
+				validator: Number.isInteger
+			}
 		},
 		unitPrice: {
 			type: Number,
-			required: true
+			required: true,
+			min: 0
 		}
 	},
 	{ _id: false }
@@ -21,7 +27,8 @@ const ArticleSchema = mongoose.Schema(
 const PurchaseSchema = mongoose.Schema({
 	clientCode: {
 		type: String,
-		required: true
+		required: true,
+		match: /C[0-9]{6}/
 	},
 	articles: {
 		type: [ArticleSchema],
@@ -29,15 +36,19 @@ const PurchaseSchema = mongoose.Schema({
 	},
 	currency: {
 		type: Number,
-		required: true
+		required: true,
+		enum: [1, 2]
 	},
 	total: {
 		type: Number,
-		required: true
+		required: true,
+		min: 0
 	},
 	tax: {
 		type: Number,
-		required: true
+		required: true,
+		min: 0,
+		max: 1
 	},
 	date: {
 		type: Date,
