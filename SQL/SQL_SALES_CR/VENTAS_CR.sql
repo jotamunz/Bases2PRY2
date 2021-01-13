502f86bf67c8146b4fd019a3c53cacfab72344ce
Joseph <jostenoriop@gmail.com>
2021-01-13 05:30:35 -0600

    Created xml package, added replication script

--- SQL/SQL_SALES_CR/VENTAS_CR.sql
+++ SQL/SQL_SALES_CR/VENTAS_CR.sql
@@ -1,23 +1,21 @@
-﻿	CREATE TABLE [dbo].[VENTAS_CR]
-(
-	[Id] INT NOT NULL, 
-    [Factura] INT NOT NULL, 
-    [Fecha] DATE NOT NULL, 
-    [FechaVencimiento] DATE NOT NULL, 
-    [Cliente] NCHAR(5) NOT NULL, 
-    [Producto ] NCHAR(6) NOT NULL, 
-    [Cantidad] INT NOT NULL, 
-    [Moneda] NCHAR(3) NOT NULL, 
-    [Precio] DECIMAL(20, 4) NOT NULL, 
-    [Total] DECIMAL(20, 4) NOT NULL, 
-    [Almacen] INT NOT NULL, 
-    [Vendedor] DECIMAL(5) NOT NULL, 
-    [TotalUSD] DECIMAL(20, 4) NOT NULL, 
-    [Impuesto] DECIMAL(20, 4) NOT NULL, 
-    [ImpuestoUSD] DECIMAL(20, 4) NOT NULL, 
-    [TipoDeCambio] DECIMAL(20, 4) NOT NULL, 
-    [Ganancia] DECIMAL(20, 4) NOT NULL, 
-    [GananciaUSD] DECIMAL(20, 4) NOT NULL,
-    CONSTRAINT PK_VENTAS PRIMARY KEY (Id)
-)
+﻿CREATE TABLE [dbo].[VENTAS] (
+    [Id]               INT             IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
+    [Factura]          INT             NOT NULL,
+    [Fecha]            DATE            NOT NULL,
+    [FechaVencimiento] DATE            NOT NULL,
+    [Cliente]          NCHAR (5)       NOT NULL,
+    [Producto]         NCHAR (7)       NOT NULL,
+    [Cantidad]         INT             NOT NULL,
+    [Moneda]           NCHAR (3)       NOT NULL,
+    [Precio]           DECIMAL (30, 5) NOT NULL,
+    [Total]            DECIMAL (30, 5) NOT NULL,
+    [Almacen]          INT             NOT NULL,
+    [Vendedor]         DECIMAL (5)     NOT NULL,
+    [TotalUSD]         DECIMAL (30, 5) NOT NULL,
+    [Impuesto]         DECIMAL (30, 5) NOT NULL,
+    [ImpuestoUSD]      DECIMAL (30, 5) NOT NULL,
+    [TipoDeCambio]     DECIMAL (30, 5) NOT NULL,
+    [Ganancia]         DECIMAL (30, 5) NOT NULL,
+    [GananciaUSD]      DECIMAL (30, 5) NOT NULL
+);
 
