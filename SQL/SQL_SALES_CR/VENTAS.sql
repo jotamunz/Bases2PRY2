a4096099b87f84bf2ca64d73f949a4aba1b15760
Joseph <jostenoriop@gmail.com>
2021-01-13 17:26:38 -0600

    XML package ready

---
+++ SQL/SQL_SALES_CR/VENTAS.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[VENTAS] (
+    [Id]               INT             IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
+    [Factura]          INT             NOT NULL,
+    [Fecha]            DATE            NOT NULL,
+    [FechaVencimiento] DATE            NOT NULL,
+    [Cliente]          NCHAR (5)       NOT NULL,
+    [Producto]         NCHAR (7)       NOT NULL,
+    [Cantidad]         INT             NOT NULL,
+    [Moneda]           NCHAR (3)       NOT NULL,
+    [Precio]           DECIMAL (30, 5) NOT NULL,
+    [Total]            DECIMAL (30, 5) NOT NULL,
+    [Almacen]          INT             NOT NULL,
+    [Vendedor]         DECIMAL (5)     NOT NULL,
+    [TotalUSD]         DECIMAL (30, 5) NOT NULL,
+    [Impuesto]         DECIMAL (30, 5) NOT NULL,
+    [ImpuestoUSD]      DECIMAL (30, 5) NOT NULL,
+    [TipoDeCambio]     DECIMAL (30, 5) NOT NULL,
+    [Ganancia]         DECIMAL (30, 5) NOT NULL,
+    [GananciaUSD]      DECIMAL (30, 5) NOT NULL
+);
+
+
