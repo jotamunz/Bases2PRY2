7f3a94668f0caa98d7ca854e56e2cd4f9317b99e
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2021-01-19 15:25:58 -0600

    successful query for colones values

--- Mongo Atlas/API/sync.js
+++ Mongo Atlas/API/sync.js
@@ -9,52 +9,73 @@ async function syncDatabase() {
 			{ synced: false },
 			{ _id: 0, __v: 0 }
 		);
-		console.log(purchases);
+		try {
+			let conn = await sql.connect(config);
+			let params =
+				'(ArticleID, SellerID, ClientID, TimeID, Quantity, UnitPriceColones, TotalColones, ProfitColones, TaxColones, UnitPriceDollars, TotalDollars, ProfitDollars, TaxDollars)';
+			for (let key in purchases) {
+				if (purchases.hasOwnProperty(key)) {
+					let purchase = purchases[key];
+					let date = moment(purchase.date).format('YYYY-MM-DD');
+					let clientCode = purchase.clientCode.replace('00', '');
+					for (let key in purchase.articles) {
+						if (purchase.articles.hasOwnProperty(key)) {
+							let article = purchase.articles[key];
+							let articleCode = article.articleCode.replace('A', '0');
+							let valueArticle =
+								"(SELECT ID FROM DIM_ARTICLE WHERE ProvitionalCode = '" +
+								articleCode +
+								"'),";
+							let valueSeller =
+								'(SELECT ID FROM DIM_SELLER WHERE SellerCode = -1),';
+							let valueClient =
+								"(SELECT ID FROM DIM_CLIENT WHERE ClientCode = '" +
+								clientCode +
+								"'),";
+							let valueTime =
+								"(SELECT ID FROM DIM_TIME WHERE [Date] = CAST('" +
+								date +
+								"' AS Date)),";
+							try {
+								await conn.query(
+									'INSERT INTO FACT_SALES' +
+										params +
+										' VALUES (' +
+										valueArticle +
+										valueSeller +
+										valueClient +
+										valueTime +
+										article.quantity.toString() +
+										',' +
+										article.unitPrice.toString() +
+										',' +
+										article.articleTotal.toString() +
+										',' +
+										article.profit.toString() +
+										',' +
+										article.tax.toString() +
+										',' +
+										'0, 0, 0, 0)'
+								);
+							} catch (error) {
+								console.log('The following article failed to sync:');
+								console.log(article);
+							}
+						}
+					}
+				}
+			}
+			conn.close();
+			console.log('Data Warehouse sync completed');
+		} catch (error) {
+			// Azure error
+			console.log(error);
+		}
+		// Update synced value
 	} catch (error) {
+		// Atlas error
 		console.log(error);
 	}
-	/*
-	try {
-		let conn = await sql.connect(config);
-		let date = moment(purchase.date).format('YYYY-MM-DD');
-		let clientCode = purchase.clientCode.replace('00', '');
-		for (let key in purchase.articles) {
-			if (purchase.articles.hasOwnProperty(key)) {
-				article = purchase.articles[key];
-				let articleCode = article.articleCode.replace('A', '0');
-				let params =
-					'(ArticleID, SellerID, ClientID, TimeID, Quantity, UnitPriceColones, TotalColones, ProfitColones, TaxColones, UnitPriceDollars, TotalDollars, ProfitDollars, TaxDollars)';
-				let valueArticle =
-					"(SELECT ID FROM DIM_ARTICLE WHERE ProvitionalCode = '" +
-					articleCode +
-					"'),";
-				let valueSeller = '(SELECT ID FROM DIM_SELLER WHERE SellerCode = -1),';
-				let valueClient =
-					"(SELECT ID FROM DIM_CLIENT WHERE ClientCode = '" +
-					clientCode +
-					"'),";
-				let valueTime =
-					"(SELECT ID FROM DIM_TIME WHERE [Date] = CAST('" +
-					date +
-					"' AS Date)),";
-				
-				await conn.query(
-					'INSERT INTO FACT_SALES' +
-						params +
-						' VALUES (' +
-						valueArticle +
-						valueSeller +
-						valueClient +
-						valueTime +
-						'0, 0, 0, 0, 0, 0, 0, 0, 0)'
-                );
-			}
-		}
-		conn.close();
-	} catch (error) {
-		console.log(error);
-    }
-    */
 }
 
 exports.syncDatabase = syncDatabase;
